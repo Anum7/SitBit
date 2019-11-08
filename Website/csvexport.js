423fe36799e7f117facce14bf9a3ae85d3852ddd
DylanBulfin <31492860+DylanBulfin@users.noreply.github.com>
2019-11-07 20:45:55 -0500

    Update csvexport.js

--- Website/csvexport.js
+++ Website/csvexport.js
@@ -18,26 +18,3 @@ function exportFunction(tblId, fn = 'output') {
     var csvData = exportData.tblData.csv;
     table.export2file(csvData.data, csvData.mimeType, csvData.filename, csvData.fileExtension, csvData.merges, csvData.RTL, csvData.sheetname);
 }
-/*
-<script>
-      function exportFunction() {
-	  var table = TableExport(document.getElementById('tblData'), {
-	      headers: true,
-	      footers: true,
-	      formats: ["csv"],
-	      filename: 'output',
-	      bootstrap: false,
-	      exportButtons: false,
-	      position: "bottom",
-	      ignoreRows: null,
-	      ignoreCols: null,
-	      trimWhitespace: true,
-	      RTL: false,
-	      sheetname: "id"
-	  });
-	  var exportData = table.getExportData();
-	  console.log(exportData);
-	  var csvData = exportData.tblData.csv;
-	  table.export2file(csvData.data, csvData.mimeType, csvData.filename, csvData.fileExtension, csvData.merges, csvData.RTL, csvData.sheetname);
-      }
-    </script>*/
